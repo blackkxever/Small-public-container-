61c18b8d3ca937d937184e91d6a9376cad71c859
blackkxever <no.reply.scurity.mail@gmail.com>
2024-08-11 00:24:34 -0600

    Create const-game-names.js

---
+++ const-game-names.js
@@ -0,0 +1,26 @@
+const gameNames = [
+  "Flip The Knife", "Math vs Bat", "Chroma Challenge", "Basketball Legend", "Rise Higher", "Koala Sling", 
+  "Stack The Pancakes", "Flip Jump", "Knife Dart", "Ant Smash", "Flick Basketball", "Don't Mess Up", 
+  "Giant Rabbit Run", "City Ball Dunkin", "Flip The Bottle", "Bill The Bowman", "Tennis is War", "Sky High", 
+  "Muay Thai Training", "Fill The Gap", "Oil Hunt", "Lock Master", "Drift Boss", "Snowboard Hero", "Ski Hero", 
+  "Santa City Run", "Candy Christmas", "Teen Patti", "Pocket Pac", "Penalty Kick", "Knock Down Cans", "Bricked", 
+  "Jewels Blocks Puzzle", "Spore Hunter", "Block Wood Puzzle", "Tripoly", "Jewel Crush", "Hop Stars", "Sky Castle", 
+  "Piano Tile Reflex", "Megacity Hop", "Cam & Leon Donut Hop", "Captain Snowball", "Sheep Fight", "Spiral Stairs", "Speed Pinball",
+  "Jumping Burger", "Classic Pinball", "Pro Bowling 3D", "Tic Tac Toe", "Airplane Survival", "Unicorn Blast", 
+  "Jump and Splat", "Color Pipes", "Puzzle Ball", "Mango Piggy Hero", "Super Jump", "Rugby Extreme", "Ships & Monsters", 
+  "2048 Dragon Island", "Leave Me Alone", "Zero Collision", "Mini Golf Adventure", "Piggy vs Bad Veggies", "Stack The Burger", 
+  "Karate Chop Kick", "Santa Airlines", "Christmas Tree Fun", "Monsters and Cake", "Cross The Bridge", "Whack A Mole", 
+  "Brick Breaker", "Mummy Hunter", "Flappy Unicorn", "Escape from Aztec", "Echo Simon", "Pro Cricket Champion", "Hexable", 
+  "Jetpack Blast", "Count Faster!", "Duck Carnival Shoot", "Paper Planes", "Numbers Challenge", "Laddu Champion", 
+  "Curiosa vs Robots", "Expert Goalkeeper", "Sushi Sensei", "Street Shooter", "Santa Chimney", "Soccer Pro", "Dinosaur Run", 
+  "Alpha Space Invasion", "Catch the Tiger", "Wham-O Slide Party", "Fast Lane Racing", "Duck Hunter", "Hangman", 
+  "Dodgeball Battle", "Gravity Kid", "Boat Dash", "Ice Hockey Shootout", "Pepperoni Gone Wild", "Finger Spinner", 
+  "Swing Forward", "Flappy Lamp", "Trixology", "Zombie Survival", "Pro Wrestling Action"
+];
+
+function getGameNames() {
+  return gameNames;
+}
+
+// Example usage: logging game names to the console
+console.log(getGameNames());
